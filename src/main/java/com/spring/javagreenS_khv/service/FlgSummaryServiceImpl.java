e05be2581b339edb07b4be953b8e1e99b0b8452e
KimWhiteStar <peculiar.25@hanmail.net>
2022-07-15 18:02:12 +0900

    모든 화면에서 사용하는 비일률적 FLAG들을 FLG_SUMMARY테이블로 정리 수정

--- src/main/java/com/spring/javagreenS_khv/service/FlgSummaryServiceImpl.java
+++ src/main/java/com/spring/javagreenS_khv/service/FlgSummaryServiceImpl.java
@@ -20,19 +20,39 @@ public class FlgSummaryServiceImpl implements FlgSummaryService {
 	@Override
 	public FlgSummaryVO searchFlg(String flgCd, String menuCd, String subMenuCd) {
 		FlgSummaryDTO flgSummaryDto = flgSummaryDao.searchFlg(flgCd, menuCd, subMenuCd);
-		FlgSummaryVO flgSummaryVo = new FlgSummaryVO();
 		String flgNm = flgSummaryDto.getFlg_nm();
-		String[] flgNmRec = flgNm.split("|");
-		String[] flgNmColumn = new String[flgNmRec.length];
+
+		int startIdx = 0, endIdx = 0, cnt = 1;
+		for (int i=0; i<flgNm.length(); i++) {
+			endIdx = flgNm.indexOf("|", startIdx);//NONE:해당안됨|OVER:30일경과|PRAC:30일미경과
+			if (-1 == endIdx) break;
+			startIdx = endIdx;
+			cnt ++;
+		}
+		startIdx = 0; 
+		endIdx = 0;
+		String[] flgNmRec = new String[cnt];//[NONE:해당안됨][OVER:30일경과][PRAC:30일미경과
+		for (int i=0; i<flgNm.length(); i++) {
+			endIdx = flgNm.indexOf("|", startIdx);//NONE:해당안됨|OVER:30일경과|PRAC:30일미경과
+			if (1 > endIdx) break;//-1,0은 안잘림
+			flgNmRec[i] = flgNm.substring(startIdx, endIdx);
+			startIdx = endIdx;
+		}
 		List<FlagVO> flagVos = new ArrayList<>();
 		FlagVO flagVo = null;
+		startIdx = 0; 
+		endIdx = 0;
 		for (String flgNmRow : flgNmRec) {
-			flgNmColumn = flgNmRow.split(":");
+			endIdx = flgNmRow.indexOf(":");//NONE:해당안됨|OVER:30일경과|PRAC:30일미경과
+			if (1 > endIdx || endIdx == flgNmRow.length()-1) break;
 			flagVo = new FlagVO();
-			flagVo.setFlgCd(flgNmColumn[0]);
-			flagVo.setFlgNm(flgNmColumn[1]);
+			flagVo.setFlgCd(flgNmRow.substring(startIdx, endIdx));//key
+			startIdx = endIdx;
+			endIdx = flgNmRow.length();
+			flagVo.setFlgNm(flgNmRow.substring(startIdx, endIdx));//value
 			flagVos.add(flagVo);
 		}
+		FlgSummaryVO flgSummaryVo = new FlgSummaryVO();
 		flgSummaryVo.setFlagVos(flagVos);
 		flgSummaryVo.setDelFlag(flgSummaryDto.getDel_flag());
 		return flgSummaryVo;
